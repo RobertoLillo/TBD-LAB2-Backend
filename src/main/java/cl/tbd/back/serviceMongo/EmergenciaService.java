c4e39fc18ed58597e85b235939b38cc93228dcf2
Pancho Rousseau <Panchojrr7@gmail.com>
2020-09-16 23:23:04 -0300

    Servicio MongoDB Terminado

---
+++ src/main/java/cl/tbd/back/serviceMongo/EmergenciaService.java
@@ -0,0 +1,35 @@
+package cl.tbd.back.serviceMongo;
+
+import cl.tbd.back.modelMongo.Emergencia;
+import cl.tbd.back.repositoryMongo.EmergenciaRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class EmergenciaService {
+
+    private final EmergenciaRepository emergenciaRepository;
+    EmergenciaService(EmergenciaRepository emergenciaRepository){
+        this.emergenciaRepository = emergenciaRepository;
+    }
+
+    @PostMapping("/mongoemergencia")
+    @ResponseBody
+    public Emergencia createEmergencia(@RequestBody Emergencia emergencia){
+        Emergencia emergenciacreada = emergenciaRepository.createEmergencia(emergencia);
+        return emergenciacreada;
+    }
+
+    @GetMapping("/mongoemergencia/count")
+    public String countEmergencia(){
+        int emergencias = emergenciaRepository.countEmergencia();
+        return String.format("Hay %s Emergencias", emergencias);
+    }
+    
+    @GetMapping("/mongoemergencia")
+    public List<Emergencia> getEmergencia(){
+        return emergenciaRepository.getEmergencia();
+    }
+}
---
+++ src/main/java/cl/tbd/back/serviceMongo/TareaService.java
@@ -0,0 +1,35 @@
+package cl.tbd.back.serviceMongo;
+
+import cl.tbd.back.modelMongo.Tarea;
+import cl.tbd.back.repositoryMongo.TareaRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class TareaService {
+    
+    private final TareaRepository tareaRepository;
+    TareaService(TareaRepository tareaRepository){
+        this.tareaRepository = tareaRepository;
+    }
+
+    @PostMapping("/mongotarea")
+    @ResponseBody
+    public Tarea createTarea(@RequestBody Tarea tarea){
+        Tarea tareacreada = tareaRepository.createTarea(tarea);
+        return tareacreada;
+    }
+
+    @GetMapping("/mongotarea/count")
+    public String countTarea(){
+        int tareas = tareaRepository.countTarea();
+        return String.format("Hay %s tareas", tareas);
+    }
+
+    @GetMapping("/mongotarea")
+    public List<Tarea> getTarea(){
+        return tareaRepository.getTarea();
+    }
+}
